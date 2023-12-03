152c422be1bdbbd5c05eac592d5050578fdc15ae
BenMeie <crazycowmm@gmail.com>
2023-12-02 22:36:31 -0800

    discord oauth updates

--- auth/routes.ts
+++ auth/routes.ts
@@ -2,6 +2,7 @@ import { app } from "../index.js";
 import bcrypt from "bcrypt";
 import { User } from "./types.js";
 import { Database } from "../db/connect.js";
+import { Request } from "express";
 const saltRounds = 10;
 
 export function initializeAuthRoutes() {
@@ -33,10 +34,42 @@ export function initializeAuthRoutes() {
         })
     })
 
-    app.get('/auth/signUpWithProvider', (req, res) => {
-        let user: User;
+    app.get('/auth/oauth_handler', (req, res) => {
+        let referer = req.headers.referer;
 
-        console.log(req)
-        res.sendStatus(200)
+        if(referer === "https://discord.com") {
+            if(req.query.code) {
+                signInWithDiscord(req.query.code as string)
+            }
+
+        }
     })
+}
+
+async function signInWithDiscord(code: string) {
+    let res = await fetch('https://discord.com/api/oauth2/token', {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'client_id': "882869275063386153",
+            'client_secret': "iRLt58vpsYscUVpePGAurWaWgnXNucfB",
+            'grant-type': 'authorization_code',
+            'code': code,
+            'redirect_uri': 'https://api.mccreations.net/auth/oauth_handler'
+        }).toString(),
+        method: 'POST'
+    })
+    let data = await res.json();
+    let access_token = data.access_token;
+    let token_type = data.token_type
+
+    res = await fetch('https://discord.com/api/users/@me', {
+        headers: {
+            authorization: `${token_type} ${access_token}`
+        }
+    })
+    let discordUser = await res.json();
+
+    console.log(discordUser)
 }
