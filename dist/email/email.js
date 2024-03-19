53e4ab3cbb894da917a5fd823f38850cd2b16505
BenMeie <crazycowmm@gmail.com>
2024-03-18 20:57:01 -0700

    add approval emails

--- dist/email/email.js
+++ dist/email/email.js
@@ -10,11 +10,11 @@ export function email(to, subject, content) {
         text: content
     });
 }
-function sendEmailTemplate(to, template, variables) {
+function sendEmailTemplate(to, template, subject, variables) {
     mg.messages.create('mail.mccreations.net', {
         from: 'MCCreations <no-reply@mccreations.net>',
         to: to,
-        subject: "Password reset for " + to,
+        subject: subject,
         template: template,
         "t:email": to,
         't:variables': JSON.stringify(variables)
@@ -24,12 +24,33 @@ function sendEmailTemplate(to, template, variables) {
 }
 export function forgotPasswordEmail(to, resetToken) {
     try {
-        sendEmailTemplate(to, "forgot_password", {
+        sendEmailTemplate(to, "forgot_password", "Password Reset for " + to, {
             email: to,
             resetLink: "https://next.mccreations.net/reset_password?token=" + resetToken
         });
     }
     catch (e) {
         throw e;
+    }
+}
+export function requestApprovalEmail(link) {
+    try {
+        sendEmailTemplate("crazycowmm@gmail.com", "request_approval", "New Map Requesting Approval", {
+            previewContent: link
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+export function approvedEmail(to, link, title) {
+    try {
+        sendEmailTemplate(to, "approved", title + " Has Been Approved!", {
+            contentLink: link,
+            contentTitle: title
+        });
+    }
+    catch (e) {
+        console.log(e);
     }
 }
