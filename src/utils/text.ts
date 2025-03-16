9f0c76c3a3460f7e33d6db3597e5423a8a55e566
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-03-16 14:12:29 -0700

    fix naughty word detection

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -5,14 +5,15 @@ const htmlRegex = /<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi
 export function containsProfanity(text: string) {
     text = text.replace(htmlRegex, "")
 
+    let found = false
     Object.keys(naughtyWords).forEach((language) => {
         if(language in naughtyWords) {
             naughtyWords[language as keyof typeof naughtyWords].forEach((word: string) => {
                 if(text.includes(word)) {
-                    return true
+                    found = true
                 }
             })
         }
     })
-    return false
+    return found
 }
